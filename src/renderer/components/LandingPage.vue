bc4993d10bc319c19ca0368370b9596cfed40db4
李隽文 <617450941@qq.com>
2017-11-04 11:09:44 +0800

    initialization

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Form :model="formItem" :label-width="80">
+      <FormItem label="Input">
+        <Input v-model="formItem.input" placeholder="Enter something..."></Input>
+      </FormItem>
+    </Form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page',
+    data () {
+      return {
+        formItem: {
+          input: '',
+          select: '',
+          radio: 'male',
+          checkbox: [],
+          switch: true,
+          date: '',
+          time: '',
+          slider: [20, 50],
+          textarea: ''
+        }
+      };
+    },
+    components: {
+    },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link);
+      }
+    }
+  };
+</script>
+
+<style>
+</style>
