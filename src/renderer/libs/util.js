29b1111c2d27a22497fc2d144e43303fce0f0f4a
李隽文 <617450941@qq.com>
2017-11-05 15:16:31 +0800

    增加ajax配置初始文件

---
+++ src/renderer/libs/util.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import env from '../config/env';
+
+let util = {};
+
+util.ajaxUrl = env === 'development' ? 'http://192.168.1.69:81/api.php' : env === 'production' ? 'http://wxyx.lyfz.net/api.php' : 'http://wxyx.lyfz.net/api.php';
+
+util.ajax = axios.create({
+  baseURL: util.ajaxUrl,
+  timeout: 30000
+});
+
+// axios请求拦截器
+util.ajax.interceptors.request.use(function (config) {
+  config.headers.token = JSON.parse(localStorage.getItem('userInfo')) === null ? null : JSON.parse(localStorage.getItem('userInfo')).token;
+  return config;
+}, function (error) {
+  return error;
+});
+
+// axios响应拦截器
+util.ajax.interceptors.response.use(function (response) {
+  if (response.data.meta.code === 6001) {
+    window.localStorage.removeItem('userInfo');
+    window.location.href = 'http://' + window.location.host;
+  }
+  return response;
+}, function (error) {
+  return error;
+});
+
+export default util;
+
+let ajax = util.ajax;
+export {
+  ajax
+};
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,17 +1,21 @@
 import Vue from 'vue';
-import axios from 'axios';
 import App from './App';
 import router from './router';
 import store from './store';
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 import './less/index.less';
+import Util from './libs/util';
+import api from './api';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
-Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
 Vue.use(iView);
+
+Vue.prototype.ajax = api;
+
+Vue.prototype.ajaxUrl = Util.ajaxUrl;
 
 /* eslint-disable no-new */
 new Vue({
