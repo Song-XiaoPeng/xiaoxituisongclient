b2ff01774185df83b7d074e0648b6fa51cb84443
李隽文 <617450941@qq.com>
2017-12-05 10:48:24 +0800

    修改菜单配置数据

--- src/renderer/config/menu.js
+++ src/renderer/config/menu.js
@@ -1,14 +1,16 @@
 let menuData = [
   {
-    menuTitle: '首页',
+    model_id: -1,
+    menuTitle: '系统首页',
     iconName: 'ios-home',
     son: {
-      sonTitle: '商品管理',
+      sonTitle: '概况数据',
       sonList: [
         {
-          menuTitle: '概况',
+          model_id: -1,
+          menuTitle: '概况数据',
           route: 'survey',
-          routeBreadcrumb: ['首页', '概况数据'],
+          routeBreadcrumb: ['系统首页', '概况数据'],
           sonMenu: []
         }
       ]
@@ -18,28 +20,18 @@ let menuData = [
     sonMenu: []
   },
   {
+    model_id: 1,
     menuTitle: '客服接待',
     iconName: 'chatbubbles',
     son: {
-      sonTitle: '客服',
+      sonTitle: '客服接待',
       sonList: [
         {
-          menuTitle: '客服',
+          model_id: 1,
+          menuTitle: '接待沟通',
           route: 'ServeIndex',
           routeBreadcrumb: [],
           sonMenu: []
-        },
-        {
-          menuTitle: '添加子账号人员',
-          route: 'staff',
-          routeBreadcrumb: [],
-          sonMenu: []
-        },
-        {
-          menuTitle: '微信客服列表',
-          route: 'serviceList',
-          routeBreadcrumb: [],
-          sonMenu: []
         }
       ]
     },
@@ -48,15 +40,31 @@ let menuData = [
     sonMenu: []
   },
   {
+    model_id: 2,
     menuTitle: '客户管理',
     iconName: 'android-contacts',
     son: {
-      sonTitle: '',
+      sonTitle: '客户管理',
       sonList: [
         {
-          menuTitle: '管理',
+          model_id: 15,
+          menuTitle: '线索',
           route: 'clientCrm',
-          routeBreadcrumb: ['管理', '客户列表'],
+          routeBreadcrumb: ['客户管理', '线索'],
+          sonMenu: []
+        },
+        {
+          model_id: 16,
+          menuTitle: '意向客户',
+          route: 'clientCrm',
+          routeBreadcrumb: ['管理', '意向客户'],
+          sonMenu: []
+        },
+        {
+          model_id: 17,
+          menuTitle: '订单客户',
+          route: 'clientCrm',
+          routeBreadcrumb: ['管理', '订单客户'],
           sonMenu: []
         }
       ]
@@ -66,12 +74,14 @@ let menuData = [
     sonMenu: []
   },
   {
+    model_id: 3,
     menuTitle: '跟踪提醒',
     iconName: 'android-alarm-clock',
     son: {
       sonTitle: '跟踪提醒',
       sonList: [
         {
+          model_id: 22,
           menuTitle: '提醒列表',
           route: 'remind',
           routeBreadcrumb: ['设置', '店铺信息'],
@@ -84,13 +94,15 @@ let menuData = [
     sonMenu: []
   },
   {
-    menuTitle: '监控',
+    model_id: 4,
+    menuTitle: '沟通监控',
     iconName: 'ios-videocam',
     son: {
-      sonTitle: '监控',
+      sonTitle: '沟通监控',
       sonList: [
         {
-          menuTitle: '监控聊天',
+          model_id: 21,
+          menuTitle: '客服交流监控',
           route: 'monitoring',
           routeBreadcrumb: [],
           sonMenu: []
@@ -102,30 +114,35 @@ let menuData = [
     sonMenu: []
   },
   {
+    model_id: 5,
     menuTitle: '数据分析',
     iconName: 'stats-bars',
     son: {
       sonTitle: '访问分析',
       sonList: [
         {
-          menuTitle: '访问分析',
+          model_id: 6,
+          menuTitle: '访问情况分析',
           route: 'visit',
           routeBreadcrumb: [],
           sonMenu: []
         },
         {
-          menuTitle: '粉丝分析',
+          model_id: 7,
+          menuTitle: '粉丝数量分析',
           route: 'fans',
           routeBreadcrumb: [],
           sonMenu: []
         },
         {
-          menuTitle: '工作量分析',
+          model_id: 8,
+          menuTitle: '工作质量分析',
           route: 'workload',
           routeBreadcrumb: [],
           sonMenu: []
         },
         {
+          model_id: 9,
           menuTitle: '质量分析',
           route: 'quality',
           routeBreadcrumb: [],
@@ -138,38 +155,64 @@ let menuData = [
     sonMenu: []
   },
   {
-    menuTitle: '微信营销平台',
+    model_id: 10,
+    menuTitle: '微信营销',
     iconName: 'chatbubbles',
     son: {
       sonTitle: '授权接入',
       sonList: [
         {
+          model_id: 11,
           menuTitle: '授权接入',
           route: 'Jurisdiction',
           routeBreadcrumb: [],
           sonMenu: []
         },
         {
+          model_id: 12,
           menuTitle: '增强功能',
           route: 'enhance',
           routeBreadcrumb: [],
           sonMenu: []
         },
         {
+          model_id: 13,
           menuTitle: '带参二维码',
           route: 'yard',
           routeBreadcrumb: [],
           sonMenu: []
         },
         {
-          menuTitle: '群发',
+          model_id: 14,
+          menuTitle: '图文群发',
           route: 'Mass',
           routeBreadcrumb: [],
           sonMenu: []
         },
         {
+          model_id: 23,
           menuTitle: '同步粉丝',
           route: 'wxfans',
+          routeBreadcrumb: [],
+          sonMenu: []
+        }
+      ]
+    },
+    route: [],
+    routeBreadcrumb: [],
+    sonMenu: []
+  },
+  {
+    model_id: 19,
+    menuTitle: '系统设置',
+    iconName: 'gear-b',
+    son: {
+      sonTitle: '子账号管理',
+      sonList: [
+        {
+          model_id: 20,
+          menuTitle: '子账号管理',
+          route: 'staff',
           routeBreadcrumb: [],
           sonMenu: []
         }
