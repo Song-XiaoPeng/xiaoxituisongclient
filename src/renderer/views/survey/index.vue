070c5fd905c9186874068b3702b1a86c0c8ea9ee
李隽文 <617450941@qq.com>
2017-12-14 19:24:01 +0800

    增加首页图

--- src/renderer/views/survey/index.vue
+++ src/renderer/views/survey/index.vue
@@ -116,101 +116,104 @@
 
 <template>
   <div id="index">
-      <div class="user-box" style="padding: 15px 15px 0 15px">
-        <div class="head-portrait">
-          <Avatar :src="userInfo.logo" style="height: 100%;width: 100%;" icon="person" size="large" />
-        </div>
-        <div class="portrait-txt-box">
-          <div class="tle">{{userInfo.company_name}}</div>
-          <div class="txt">
-            <span class="icon"><Icon type="ios-telephone"></Icon></span>
-            <span>{{userInfo.phone_no}}</span>
-            <span class="icon"><Icon type="social-codepen"></Icon></span>
-            <span>{{userInfo.group_name}}</span>
-            <span class="icon"><Icon type="ios-location"></Icon></span>
-            <span>{{userInfo.address}}</span>
-          </div>
+    <div class="user-box" style="padding: 15px 15px 0 15px">
+      <div class="head-portrait">
+        <Avatar :src="userInfo.logo" style="height: 100%;width: 100%;" icon="person" size="large" />
+      </div>
+      <div class="portrait-txt-box">
+        <div class="tle">{{userInfo.company_name}}</div>
+        <div class="txt">
+          <span class="icon"><Icon type="ios-telephone"></Icon></span>
+          <span>{{userInfo.phone_no}}</span>
+          <span class="icon"><Icon type="social-codepen"></Icon></span>
+          <span>{{userInfo.group_name}}</span>
+          <span class="icon"><Icon type="ios-location"></Icon></span>
+          <span>{{userInfo.address}}</span>
         </div>
       </div>
-      <div class="count-box" v-if="is_onlineService_show" style="padding: 15px">
-        <div class="count-card" >
-          <div style="background-color: #83bafc">
-            <img src="~@/assets/images/survey1.png" alt="" style="margin-top: 15px">
-          </div>
-          <div style="height: 80px;margin-top: 10px">
-             <div>总客服</div>
-             <div style="font-weight: bold">{{onlineServiceData.customer_service_total.total}}</div>
-           </div>
-           <div style="height: 80px;margin-top: 10px">
-             <div>在线客服</div>
-             <div style="font-weight: bold">{{onlineServiceData.customer_service_total.on_line_total}}</div>
-           </div>
-        </div>
-        <div class="count-card">
-          <div style="background-color: #78e4c6">
-            <img src="~@/assets/images/survey2.png" alt="" style="margin-top: 15px">
-          </div>
-          <div style="height: 80px;margin-top: 10px">
-            <div>在线访客</div>
-            <div style="font-weight: bold">{{onlineServiceData.visitor_total.reception_total}}</div>
-          </div>
-          <div style="height: 80px;margin-top: 10px">
-            <div>接待中</div>
-            <div style="font-weight: bold">{{onlineServiceData.visitor_total.total}}</div>
-          </div>
-        </div>
-        <div class="count-card">
-          <div style="background-color: #efbc6c">
-            <img src="~@/assets/images/survey3.png" alt="" style="margin-top: 15px">
-          </div>
-          <div style="height: 80px;margin-top: 10px">
-            <div>待响应</div>
-            <div style="font-weight: bold">{{onlineServiceData.response_time.pending_response}}</div>
-          </div>
-          <div style="height: 80px;margin-top: 10px">
-            <div>平均响应时长</div>
-            <div style="font-weight: bold">{{onlineServiceData.response_time.average_response}}</div>
-          </div>
-        </div>
-      </div>
-    <div class="table-box">
-        <div class="tab-top">
-          <div class="ranking">
-            客服排名
-          </div>
-          <div class="f-r date-sel-box">
-            <ul class="cl">
-              <li class="f-l" v-bind:class="is_tab_active == 1 ? 'active': ''" @click="selDateFun(1)" >今天</li>
-              <li class="f-l" v-bind:class="is_tab_active == 2 ? 'active': ''" @click="selDateFun(2)" >昨天</li>
-              <!--<li class="f-l" v-bind:class="is_tab_active == 3 ? 'active': ''" @click="selDateFun(3)" >近三天</li>-->
-              <li class="f-l" v-bind:class="is_tab_active == 3 ? 'active': ''" @click="selDateFun(3)" >近一周</li>
-              <li class="f-l" v-bind:class="is_tab_active == 4 ? 'active': ''" @click="selDateFun(4)" >近一月</li>
-              <li class="f-l" v-bind:class="is_tab_active == 5 ? 'active': ''" @click="selDateFun(5)" >自定义时间段</li>
-            </ul>
-          </div>
-        </div>
-        <div style="padding: 10px 15px">
-          <Table border :columns="columns1" :data="data1"></Table>
-        </div>
     </div>
-    <!--<div class="chart-box">-->
-      <!--<div class="top" >-->
-        <!--<Select v-model="model1" style="width:200px;float: right">-->
-          <!--<Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-        <!--</Select>-->
-      <!--</div>-->
-      <!--<div class="chart">-->
-          <!--&lt;!&ndash;<IEcharts :option="pie1" :resizable="true" :loading="loading"  @ready="onReady" @click="onClick"></IEcharts>&ndash;&gt;-->
-      <!--</div>-->
-    <!--</div>-->
+    <div class="count-box" v-if="is_onlineService_show" style="padding: 15px">
+      <div class="count-card" >
+        <div style="background-color: #83bafc">
+          <img src="~@/assets/images/survey1.png" alt="" style="margin-top: 15px">
+        </div>
+        <div style="height: 80px;margin-top: 10px">
+            <div>总客服</div>
+            <div style="font-weight: bold">{{onlineServiceData.customer_service_total.total}}</div>
+          </div>
+          <div style="height: 80px;margin-top: 10px">
+            <div>在线客服</div>
+            <div style="font-weight: bold">{{onlineServiceData.customer_service_total.on_line_total}}</div>
+          </div>
+      </div>
+      <div class="count-card">
+        <div style="background-color: #78e4c6">
+          <img src="~@/assets/images/survey2.png" alt="" style="margin-top: 15px">
+        </div>
+        <div style="height: 80px;margin-top: 10px">
+          <div>在线访客</div>
+          <div style="font-weight: bold">{{onlineServiceData.visitor_total.reception_total}}</div>
+        </div>
+        <div style="height: 80px;margin-top: 10px">
+          <div>接待中</div>
+          <div style="font-weight: bold">{{onlineServiceData.visitor_total.total}}</div>
+        </div>
+      </div>
+      <div class="count-card">
+        <div style="background-color: #efbc6c">
+          <img src="~@/assets/images/survey3.png" alt="" style="margin-top: 15px">
+        </div>
+        <div style="height: 80px;margin-top: 10px">
+          <div>待响应</div>
+          <div style="font-weight: bold">{{onlineServiceData.response_time.pending_response}}</div>
+        </div>
+        <div style="height: 80px;margin-top: 10px">
+          <div>平均响应时长</div>
+          <div style="font-weight: bold">{{onlineServiceData.response_time.average_response}}</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="table-box">
+      <div class="tab-top">
+        <div class="ranking">
+          客服排名
+        </div>
+        <div class="f-r date-sel-box">
+          <ul class="cl">
+            <li class="f-l" v-bind:class="is_tab_active == 1 ? 'active': ''" @click="selDateFun(1)" >今天</li>
+            <li class="f-l" v-bind:class="is_tab_active == 2 ? 'active': ''" @click="selDateFun(2)" >昨天</li>
+            <!--<li class="f-l" v-bind:class="is_tab_active == 3 ? 'active': ''" @click="selDateFun(3)" >近三天</li>-->
+            <li class="f-l" v-bind:class="is_tab_active == 3 ? 'active': ''" @click="selDateFun(3)" >近一周</li>
+            <li class="f-l" v-bind:class="is_tab_active == 4 ? 'active': ''" @click="selDateFun(4)" >近一月</li>
+            <li class="f-l" v-bind:class="is_tab_active == 5 ? 'active': ''" @click="selDateFun(5)" >自定义时间段</li>
+          </ul>
+        </div>
+      </div>
+      <div style="padding: 10px 15px">
+        <Table border :columns="columns1" :data="data1"></Table>
+      </div>
+    </div>
+    
+    <div class="table-box">
+      <div class="tab-top">
+        <div class="ranking">
+          绩效咨询
+        </div>
+        <div class="f-r date-sel-box">
+
+        </div>
+      </div>
+      <div style="padding:10px 15px; margin-bottom: 10px;">
+        <ve-line :data="chartData"></ve-line>
+      </div>
+    </div>
 
     <!-- 自定义时间段弹窗 -->
     <Modal v-model="popup1" title="自定义时间" @on-ok="customDateFun">
       <DatePicker type="daterange" confirm placement="bottom-end" placeholder="请选择时间点" style="width: 100%"  @on-change="customSelDateFun"></DatePicker>
     </Modal>
     <!-- end自定义时间段弹窗 -->
-
-
 
     <!-- 状态 -->
     <Spin fix v-if="is_Loading">
@@ -222,61 +225,76 @@
 </template>
 
 <script>
+  import VeLine from 'v-charts/lib/line';
+
   export default {
     data () {
       return {
         columns1: [
           {
             title: '客户姓名',
-            key: 'name'
+            key: 'name',
+            align: 'center'
           },
           {
             title: '会话总数',
-            key: 'session_total'
+            key: 'session_total',
+            align: 'center'
           },
           {
             title: '首次会话',
-            key: 'first_session'
+            key: 'first_session',
+            align: 'center'
           },
           {
             title: '有效会话',
-            key: 'effective'
+            key: 'effective',
+            align: 'center'
           },
           {
             title: '会话率',
-            key: 'effective_percentage'
+            key: 'effective_percentage',
+            align: 'center'
           },
           {
             title: '无效会话',
-            key: 'invalid_session'
+            key: 'invalid_session',
+            align: 'center'
           },
           {
             title: '会话遗漏',
-            key: 'session_missing'
+            key: 'session_missing',
+            align: 'center'
           },
           {
             title: '手动会话',
-            key: 'own_session'
+            key: 'own_session',
+            align: 'center'
           },
           {
             title: '自动会话',
-            key: 'auto_session'
+            key: 'auto_session',
+            align: 'center'
           },
           {
             title: '发起会话',
-            key: 'active_session'
+            key: 'active_session',
+            align: 'center'
           },
           {
             title: '采集客咨',
-            key: 'collection'
+            key: 'collection',
+            align: 'center'
           },
           {
             title: '采客咨率',
-            key: 'collection_percentage'
+            key: 'collection_percentage',
+            align: 'center'
           },
           {
             title: '消息总数',
-            key: 'send_message_total'
+            key: 'send_message_total',
+            align: 'center'
           }
         ],
         data1: [],
@@ -302,10 +320,32 @@
         userInfo: {},
         is_tab_active: 1,
         popup1: false,
-        customSelDateData: null
+        customSelDateData: null,
+        chartData: {
+          columns: ['总咨询量', '有效咨询', '无效咨询', '咨询遗漏', '采集客资'],
+          rows: [
+            { '总咨询量': 1523, '日期': '8:00', '有效咨询': 1523, '无效咨询': 1523, '采集客资': 1764 },
+            { '总咨询量': 1223, '日期': '9:00', '有效咨询': 1223, '无效咨询': 4312, '采集客资': 3314 },
+            { '总咨询量': 2123, '日期': '10:00', '有效咨询': 1593, '无效咨询': 3321, '采集客资': 3321 },
+            { '总咨询量': 4123, '日期': '11:00', '有效咨询': 3321, '无效咨询': 1333, '采集客资': 2215 },
+            { '总咨询量': 3123, '日期': '12:00', '有效咨询': 2231, '无效咨询': 3221, '采集客资': 1523 },
+            { '总咨询量': 1673, '日期': '13:00', '有效咨询': 4212, '无效咨询': 4312, '采集客资': 100 },
+            { '总咨询量': 3211, '日期': '14:00', '有效咨询': 3221, '无效咨询': 3122, '采集客资': 1673 },
+            { '总咨询量': 4312, '日期': '15:00', '有效咨询': 1333, '无效咨询': 1764, '采集客资': 3221 },
+            { '总咨询量': 2123, '日期': '16:00', '有效咨询': 1321, '无效咨询': 2231, '采集客资': 1267 },
+            { '总咨询量': 6543, '日期': '17:00', '有效咨询': 1764, '无效咨询': 3122, '采集客资': 1333 },
+            { '总咨询量': 2123, '日期': '18:00', '有效咨询': 4231, '无效咨询': 1223, '采集客资': 1764 },
+            { '总咨询量': 7123, '日期': '19:00', '有效咨询': 3122, '无效咨询': 1673, '采集客资': 4212 }
+          ]
+        },
+        chartSettings: {
+          metrics: ['总咨询量', '有效咨询', '无效咨询', '咨询遗漏', '采集客资'],
+          dimension: ['日期']
+        }
       };
     },
     components: {
+      VeLine
     },
     watch: {
     },
